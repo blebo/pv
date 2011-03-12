9017909055dd701c2ab5ff4a8f3ecf921993dd36
Edmund Tse <tseedmund@gmail.com>
2011-03-12 19:28:52 +1100

    Added setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pv',
+	version='0.2',
+	description='PV Inverter Monitoring Library',
+	author='Edmund Tse',
+	author_email='tseedmund@gmail.com',
+	url='http://pv.codeplex.com/',
+	py_modules=['pv.pvoutput', 'pv.cms'],
+	)
