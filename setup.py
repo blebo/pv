08d80fbd5db3c41812b07cb5ed656fd82213a820
Adam Gibson <adam.gibson@tc4k.net>
2014-06-22 00:54:04 +0800

    Fix indents

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='pv',
-	version='0.2',
-	description='PV Inverter Monitoring Library',
-	author='Edmund Tse',
-	author_email='tseedmund@gmail.com',
-	url='http://pv.codeplex.com/',
-	py_modules=['pv.pvoutput', 'pv.cms'],
-	)
+setup(
+    name='pv',
+    version='0.2',
+    description='PV Inverter Monitoring Library',
+    author='Edmund Tse',
+    author_email='tseedmund@gmail.com',
+    url='http://pv.codeplex.com/',
+    py_modules=['pv.pvoutput', 'pv.cms'],
+)
